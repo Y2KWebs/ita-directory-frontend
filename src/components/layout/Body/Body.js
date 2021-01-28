1e7f753fb7ddf534e25ae3ec68fe3490ff58b9ec
dianavile <diana.vile@gmail.com>
2021-01-28 20:18:38 +0100

    inspect files for localStorage

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Header from 'components/layout/Header/Header'
-import Footer from 'components/layout/Footer/Footer'
-import { Childrens, StyledBody} from './styles'
+import React from "react";
+import Header from "components/layout/Header/Header";
+import Footer from "components/layout/Footer/Footer";
+import {Childrens, StyledBody} from "./styles";
 
-const Body = ({ children, title }) => {
-  return (
-    <StyledBody>
-      <Header title={title} />
-      <Childrens>
-        {children}
-      </Childrens>
-      <Footer />
-    </StyledBody>
-  )
-}
+const Body = ({children, title}) => {
+	return (
+		<StyledBody>
+			<Header title={title} />
+			<Childrens>{children}</Childrens>
+			<Footer />
+		</StyledBody>
+	);
+};
 
-export default Body
+export default Body;
+
+//TODO: CHECK LOGIN OF USER
+//check if user is logged in, if true, return redirect to protectedroute from index.js in composed/protectedRoute
--- src/components/layout/Body/styles.js
+++ src/components/layout/Body/styles.js
@@ -1,14 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const StyledBody = styled.div`
-    margin: 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-`
+	margin: 0;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+`;
 
 export const Childrens = styled.div`
-    display: flex;
-    justify-content: center;
-    padding-top: 3.5rem
-`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding-top: 3.5rem;
+`;
+
+//center items with align-items:center;
