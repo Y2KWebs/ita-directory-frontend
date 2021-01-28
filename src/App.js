f380af6028807135ba4661e916f34846873bafd3
dianavile <diana.vile@gmail.com>
2021-01-28 08:30:11 +0100

    fix error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React, {useState} from "react";
-import Header from "components/layout/Header/Header";
-import {Route, Switch} from "react-router-dom";
-// import AsyncButton from "components/units/AsyncButton";
-// import {faEye} from "@fortawesome/free-solid-svg-icons";
-=======
 import React, { useState } from 'react'
 import Header from 'components/layout/Header/Header'
 // import AsyncButton from "components/units/AsyncButton";
 // import {faEye} from "@fortawesome/free-solid-svg-icons";
 import { Route, Switch } from 'react-router-dom'
->>>>>>> 3b2af41ee916790b70698cb5a2fc044dcf15ebc5
 // import ProtectedRoute from "components/composed/ProtectedRoute";
 
 // Pages
@@ -18,16 +10,9 @@ import { Route, Switch } from 'react-router-dom'
 // import Page404 from "screens/404";
 
 // Userflow
-<<<<<<< HEAD
-import Login from "screens/UserFlow/Login/Login";
-import Registration from "screens/UserFlow/Registration/Registration";
-import Profile from "screens/UserFlow/Profile/Profile";
-import Footer from "components/layout/Footer/Footer";
-=======
 import Login from 'screens/UserFlow/Login/Login'
 import Registration from 'screens/UserFlow/Registration/Registration'
 import Footer from 'components/layout/Footer/Footer'
->>>>>>> 3b2af41ee916790b70698cb5a2fc044dcf15ebc5
 // import RecoverPassword from "screens/UserFlow/RecoverPassword";
 
 // Input
@@ -35,34 +20,6 @@ import Footer from 'components/layout/Footer/Footer'
 /* import axios from "axios"; */
 
 const App = () => {
-<<<<<<< HEAD
-	const [view, setView] = useState("");
-	const [token, setToken] = useState();
-
-	const handleLogin = (token) => {
-		setToken(token);
-		setView("home");
-	};
-	return (
-		<>
-			<Header />
-			<Switch>
-				{/* Userflow */}
-				<Route exact path="/login" component={Login} />
-				{/* <Route exact path="/login" component={Login}> */}
-				{/* <Login onLogin={handleLogin} /> */}
-				<Route exact path="/profile" component={Profile} />
-				<Route exact path="/registration" component={Registration} />
-				{/* <Route exact path="/recover-password/:hash" component={RecoverPassword} /> */}
-				{/* Caregiver */}
-				{/* <ProtectedRoute exact path="/" component={Home} /> */}
-				{/* <ProtectedRoute component={Page404} /> */}
-			</Switch>
-			<Footer />
-		</>
-	);
-};
-=======
   const [view, setView] = useState('')
   const [token, setToken] = useState()
 
@@ -86,6 +43,5 @@ const App = () => {
     </>
   )
 }
->>>>>>> 3b2af41ee916790b70698cb5a2fc044dcf15ebc5
 
 export default App
